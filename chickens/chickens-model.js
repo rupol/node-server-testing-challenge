5cf95de9f2e1746629adfe8b4ea2d5301478cb72
Ruth Poliakon <r.poliakon@gmail.com>
2020-01-23 19:58:35 -0700

    stretch - remaining chickens-model crud functions and tests

--- chickens/chickens-model.js
+++ chickens/chickens-model.js
@@ -1,11 +1,29 @@
 const db = require("../data/config");
 
+function get() {
+  return db("chickens");
+}
+
+function getById(id) {
+  return db("chickens")
+    .where({ id })
+    .first();
+}
+
 async function create(chicken) {
   const [id] = await db("chickens").insert(chicken);
 
   return db("chickens")
     .where({ id })
     .first();
+}
+
+async function update(id, changes) {
+  await db("chickens")
+    .where({ id })
+    .update(changes);
+
+  return getById(id);
 }
 
 function del(id) {
@@ -15,6 +33,9 @@ function del(id) {
 }
 
 module.exports = {
+  get,
+  getById,
   create,
+  update,
   del
 };
--- chickens/chickens-model.spec.js
+++ chickens/chickens-model.spec.js
@@ -6,6 +6,16 @@ beforeEach(async () => {
 });
 
 describe("chickens model", () => {
+  test("get", async () => {
+    const res = await chickensModel.get();
+    expect(res).toHaveLength(5);
+  });
+
+  test("getById", async () => {
+    const res = await chickensModel.getById(1);
+    expect(res.name).toMatch(/carol/i);
+  });
+
   test("create", async () => {
     await chickensModel.create({ name: "janelle", breed: "salmon faverolle" });
     const chickens = await db("chickens");
@@ -16,5 +26,16 @@ describe("chickens model", () => {
     await chickensModel.del(1);
     const chickens = await db("chickens");
     expect(chickens).toHaveLength(4);
+  });
+
+  test("update", async () => {
+    const res = await chickensModel.update(1, {
+      name: "janelle",
+      breed: "salmon faverolle"
+    });
+    expect(res.name).toMatch(/janelle/i);
+
+    const chickens = await db("chickens");
+    expect(chickens).toHaveLength(5);
   });
 });
